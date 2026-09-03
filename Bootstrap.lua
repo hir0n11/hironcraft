c38a6ea26d31ea44d4c0e010c300bae9f1022eff
hironPC <jion.ru.08@mail.ru>
2026-09-04 00:48:05 +0300

    Stabilize quick reply toast layout

--- Bootstrap.lua
+++ Bootstrap.lua
@@ -4,7 +4,7 @@ HironCraftProfit = HironCraftProfit or {}
 HironCraft = HironCraftProfit
 
 HironCraft.addonName = addonName
-HironCraft.version = "0.2.19"
+HironCraft.version = "0.2.20"
 HironCraft.migration = HironCraft.migration or {
     imported = {},
 }
